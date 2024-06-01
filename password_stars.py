222737ab52a5160f0508b3fecc4afbc9349d9107
Sithuhein <sithuhein@my.jcu.edu.au>
2024-06-01 19:09:37 +0800

    password check

---
+++ password_stars.py
@@ -0,0 +1,7 @@
+password = input("Type the password: ")
+MINIMUM_LENGTH = 7
+while len(password) < MINIMUM_LENGTH:
+    print("Invalid password")
+    password = input("Type the password: ")
+asterisks = len(password) * "*"
+print(asterisks)
